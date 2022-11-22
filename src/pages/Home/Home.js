4d3c1d12914ca48157ce01f50b030692ec62261e
benwilliams2109 <ben.williams21@outlook.com>
2022-11-22 22:46:43 +0000

    still working on the homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,31 +1,22 @@
 import React from "react";
 import "./Home.css";
+import caveman from "./images/caveman.jpg";
 
 const Home = () => (
   <div className="page-content">
+    <div className="row-top">
+      <div className="left-col-top">Ben Williams</div>
+      <div className="right-col-top"></div>
+    </div>
     <div className="row">
       <div className="left-col">
-        The world is changing; companies are capturing trillions of bytes of
-        information about their customers, suppliers, and operations. This
-        explosion of data is disrupting industries and creating new
-        opportunities. Companies need people who can take data, understand it,
-        process it, extract value from it, visualise it, and communicate it.
-        They need people who deeply understand data, its potential and its
-        limitations, who can frame business problems, analyse data with
-        statistical techniques, develop and maintain predictive models, and
-        communicate analytics results to business executives, partners and
-        customers.
+        My name is Ben Williams. I am a highly ambitious and confident final
+        year Mathematics student at Loughborough University.
       </div>
       <div className="right-col">
-        {" "}
-        Business Analytics is taught by UCL School of Management. It combines
-        modules that explore how data and analytics are transforming key areas
-        of business (strategy, marketing, operations) with modules that provide
-        the statistical and computational skills needed to make effective use of
-        the latest business analytics tools. Business Analytics requires a
-        combination of management insight, strong quantitative and analytical
-        skills, and an understanding of the technology required to handle data
-        at scale. The innovative programme prepares people for leadership roles
+        <div className="right-col-image">
+          <img src={caveman} alt="caveman-coder" />
+        </div>
       </div>
     </div>
     <div className="row">
